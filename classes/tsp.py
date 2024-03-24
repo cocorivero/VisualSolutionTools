71c0c81a6bc65a096127ab52d6bc98225480c3b8
Andy Rivero <andy.rivero@avangenio.com>
2024-03-24 16:50:45 -0400

    ajustar clases

--- classes/tsp.py
+++
@@ -1,8 +0,0 @@
-from .depot import Depto
-
-
-class TSP:
-    def __init__(self, depot: Depto, locations: list, route: list):
-        self.depot = depot
-        self.locations = locations
-        self.route = route
--- problems/tsp_visual.py
+++ problems/tsp_visual.py
@@ -4,30 +4,30 @@ sys.path.append("./")
 
 import matplotlib.pyplot as plt
 
-from vrp_data import main_vrp, create_data_model
+from instances_problems import create_data_model_tsp, create_data_solution_tsp
 from services.draw_funtions import *
-from services.draw_routes import draw_vrp_route
-from classes.depot import Depto
-from classes.vehicle_routing_problem_classes import VRP
+from services.draw_routes import draw_tsp_route
+from classes.vehicle_routing_problem_classes import Depto, TSP
 
 
 # Crear el modelo de datos
-data = create_data_model()
+data = create_data_model_tsp()
+solution = create_data_solution_tsp()
 
 # Crear instancia de la clase TSP con la instancia de Depto, datos de lugares y la ruta
-vrp_problem = VRP(
-    Depto(data["locations"][data["deposito"]], data["deposito"]),
+tsp_problem = TSP(
+    Depto(data["locations"][data["depot"]], data["depot"]),
     data["locations"],
-    main_vrp(),
+    solution["route"],
 )
 
 
-def plot_nodes_and_route(depot: Depto, locations, vehicle_routes_mapping):
+def plot_nodes_and_route(depot: Depto, locations, route):
     # Graficar las ciudades y la ruta
     plt.figure("Problema del Viajante de Comercio", figsize=(8, 6))
 
     # Dibujar la ruta con flechas y los índices de los lugares
-    draw_vrp_route(locations, vehicle_routes_mapping)
+    draw_tsp_route(locations, route)
 
     # Dibujar el depósito
     # draw_deposit(tsp_problem.depot.data, tsp_problem.depot.index)
@@ -40,11 +40,11 @@ def plot_nodes_and_route(depot: Depto, locations, vehicle_routes_mapping):
     # draw_legend(locations)
 
     # Configurar el gráfico
-    draw_graph("Coordinate X", "Coordinate Y", "Vehicle Routing Problem")
+    draw_graph("Coordinate X", "Coordinate Y", "Travelling Salesperson Problem")
 
 
 plot_nodes_and_route(
-    vrp_problem.depot,
-    vrp_problem.locations,
-    vrp_problem.vehicle_routes_mapping,
+    tsp_problem.depot,
+    tsp_problem.locations,
+    tsp_problem.route,
 )
