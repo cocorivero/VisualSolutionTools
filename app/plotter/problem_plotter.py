558cc31c230caaa42b6ef570b995addde484f2da
Andy Rivero <andy.rivero@avangenio.com>
2025-02-08 12:43:14 -0500

    feat(general): remove Singleton

--- app/plotter/problem_plotter.py
+++ app/plotter/problem_plotter.py
@@ -1,5 +1,4 @@
 import sys
-from matplotlib import pyplot as plt
 from typing import List, Dict
 from app.models.vrp import VRP
 from app.factories.depot_factory import ConcreteDepotFactory
@@ -23,53 +22,10 @@ class ProblemPlotterFacade:
             "sbrp": "School Bus Routing Problem (SBRP)",
         }
 
-    def plot_problem(
-        self,
-        problem_type: str,
-        data,
-        routes: List = [],
-        depot_config: Dict = None,
-        stop_config: Dict = None,
-        routes_config: Dict = None,
-        graph_title: str = None,
-    ):
-        # Asignamos un título por defecto si no se proporciona uno
-        graph_title = graph_title or self.default_graph_titles.get(
-            problem_type, "Tipo de problema no soportado"
-        )
-
-        if graph_title == "Tipo de problema no soportado":
-            print(graph_title)
-            return
-
-        # Creamos el problema usando la configuración y los datos proporcionados
-        vrp = self._create_problem(
-            problem_type=problem_type,
-            depot_id=data.get("depot_id", ""),
-            stops=data.get("locations", {}),
-            routes=routes,
-            passengers=data.get("passengers", []),
-            depot_config=depot_config or {},
-            stop_config=stop_config or {},
-            routes_config=routes_config or {},
-        )
-
-        # Graficamos el problema si fue creado correctamente
-        if vrp:
-            vrp.draw_routes()
-            vrp.draw_stops()
-            vrp.draw_depot()
-            # vrp.draw_legend()
-            # Configuramos el gráfico y lo mostramos
-            plt.xlabel("Coordenada X")
-            plt.ylabel("Coordenada Y")
-            plt.title(graph_title)
-            plt.tight_layout()
-            plt.show()
-
     def _create_problem(
         self,
         problem_type: str,
+        problem_name: str,
         depot_id: str,
         stops: Dict,
         routes: List,
@@ -97,6 +53,7 @@ class ProblemPlotterFacade:
 
         # Creamos los argumentos para la instancia de VRP
         vrp_args = {
+            "problem_name": problem_name,
             "depot": depot,
             "stops": stops,
             "routes": routes,
@@ -108,7 +65,43 @@ class ProblemPlotterFacade:
 
         # Verificamos si el tipo de problema es soportado y creamos la instancia de VRP
         if problem_type in ["vrp", "tsp", "cvrp", "bss", "sbrp"]:
-            return VRP.get_instance(**vrp_args)
+            return VRP(**vrp_args)
         else:
             print("Tipo de problema no soportado")
             return None
+
+    def plot_problem(
+        self,
+        problem_type: str,
+        data,
+        routes: List = [],
+        depot_config: Dict = None,
+        stop_config: Dict = None,
+        routes_config: Dict = None,
+        graph_title: str = None,
+    ):
+        # Asignamos un título por defecto si no se proporciona uno
+        graph_title = graph_title or self.default_graph_titles.get(
+            problem_type, "Tipo de problema no soportado"
+        )
+
+        if graph_title == "Tipo de problema no soportado":
+            print(graph_title)
+            return
+
+        # Creamos el problema usando la configuración y los datos proporcionados
+        vrp = self._create_problem(
+            problem_type=problem_type,
+            problem_name=graph_title,
+            depot_id=data.get("depot_id", ""),
+            stops=data.get("locations", {}),
+            routes=routes,
+            passengers=data.get("passengers", []),
+            depot_config=depot_config or {},
+            stop_config=stop_config or {},
+            routes_config=routes_config or {},
+        )
+
+        # Graficamos el problema si fue creado correctamente
+        if vrp:
+            vrp.draw_problem_2d()
