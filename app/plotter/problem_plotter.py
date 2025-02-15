55a8667dad553c8c0fa2249c612fe7a38bb2c058
Andy Rivero <andy.rivero@avangenio.com>
2025-02-15 09:51:48 -0500

    feat(refactor): Update Factory Method pattern and remove typing

--- app/plotter/problem_plotter.py
+++ app/plotter/problem_plotter.py
@@ -1,20 +1,24 @@
 import sys
-from typing import List, Dict
-from app.models.vrp import VRP
-from app.factories.depot_factory import ConcreteDepotFactory
-from app.factories.stop_factory import ConcreteStopFactory
-from app.factories.route_factory import ConcreteRouteFactory
+from app.problem.factory_visualization import FactoryVisualizacion
 
 sys.path.append("./")
 
 
 class ProblemPlotterFacade:
-    def __init__(self):
-        # Inicializamos las fábricas necesarias
-        self.depot_factory = ConcreteDepotFactory()
-        self.stop_factory = ConcreteStopFactory()
-        self.route_factory = ConcreteRouteFactory()
-        self.default_graph_titles = {
+
+    def plot_problem(
+        self,
+        problem_type,
+        view_mode,
+        data,
+        routes=[],
+        depot_config=None,
+        stop_config=None,
+        routes_config=None,
+        problem_title=None,
+    ):
+        # Diccionario de títulos por defecto dentro de la función
+        default_problem_titles = {
             "tsp": "Traveling Salesman Problem (TSP)",
             "vrp": "Vehicle Routing Problem (VRP)",
             "cvrp": "Capacitated Vehicle Routing Problem (CVRP)",
@@ -22,77 +26,19 @@ class ProblemPlotterFacade:
             "sbrp": "School Bus Routing Problem (SBRP)",
         }
 
-    def _create_problem(
-        self,
-        problem_type: str,
-        problem_name: str,
-        depot_id: str,
-        stops: Dict,
-        routes: List,
-        passengers: List,
-        depot_config: Dict,
-        stop_config: Dict,
-        routes_config: Dict,
-    ) -> VRP:
-        # Definimos argumentos adicionales para la creación de paradas
-        stop_args = {
-            "with_capacity": problem_type in ["cvrp", "bss", "sbrp"],
-            "with_assigned_passengers": problem_type in ["bss", "sbrp"],
-        }
-
-        # Creamos los objetos necesarios usando las fábricas
-        depot = self.depot_factory.create_depot(
-            depot_id=depot_id, stops=stops, config=depot_config
-        )
-        stops = self.stop_factory.create_stops(
-            stop_data=stops, stop_config=stop_config, **stop_args
-        )
-        routes = self.route_factory.create_routes(
-            routes=routes, routes_config=routes_config
-        )
-
-        # Creamos los argumentos para la instancia de VRP
-        vrp_args = {
-            "problem_name": problem_name,
-            "depot": depot,
-            "stops": stops,
-            "routes": routes,
-        }
-
-        # Agregamos pasajeros si el problema lo requiere
-        if problem_type in ["bss", "sbrp"]:
-            vrp_args["passengers"] = passengers
-
-        # Verificamos si el tipo de problema es soportado y creamos la instancia de VRP
-        if problem_type in ["vrp", "tsp", "cvrp", "bss", "sbrp"]:
-            return VRP(**vrp_args)
-        else:
-            print("Tipo de problema no soportado")
-            return None
-
-    def plot_problem(
-        self,
-        problem_type: str,
-        data,
-        routes: List = [],
-        depot_config: Dict = None,
-        stop_config: Dict = None,
-        routes_config: Dict = None,
-        graph_title: str = None,
-    ):
-        # Asignamos un título por defecto si no se proporciona uno
-        graph_title = graph_title or self.default_graph_titles.get(
+        # Asignamos el título utilizando el título personalizado si se proporciona
+        problem_title = problem_title or default_problem_titles.get(
             problem_type, "Tipo de problema no soportado"
         )
 
-        if graph_title == "Tipo de problema no soportado":
-            print(graph_title)
+        if problem_title == "Tipo de problema no soportado":
+            print(problem_title)
             return
 
-        # Creamos el problema usando la configuración y los datos proporcionados
-        vrp = self._create_problem(
+        visualization = FactoryVisualizacion.crear_visualizacion(view_mode)
+        vrp = visualization.create_problem(
             problem_type=problem_type,
-            problem_name=graph_title,
+            problem_name=problem_title,
             depot_id=data.get("depot_id", ""),
             stops=data.get("locations", {}),
             routes=routes,
